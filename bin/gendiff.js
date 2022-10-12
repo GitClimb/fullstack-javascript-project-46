962eb8d83b29baffebd21e06c48ab95de9d64c81
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-10-12 20:02:59 +0800

    feat: arguments and option "format"

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,11 +2,12 @@
 
 import { program } from 'commander';
 
-program.version('0.0.1');
-program.description('Compares two configuration files and shows a difference.');
+program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.')
+    .argument('filepath1')
+    .argument('filepath2')
+    .option('-f, --format <type>', 'output format');
 
 program.parse();
-
-
-
 
