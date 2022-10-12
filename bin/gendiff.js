8094c92aa2ddc2b8f631d81728c2f48ea1291aa6
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-10-12 19:25:17 +0800

    frat: add comands gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,7 @@ import { program } from 'commander';
 program.version('0.0.1');
 program.description('Compares two configuration files and shows a difference.');
 
-program.parse(process.argv);
+program.parse();
 
 
 
