fae571246f8656e81c711142f6d08bf1e1d3fd65
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-10-14 20:00:42 +0800

    style: edit space

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,11 +3,10 @@
 import { program } from 'commander';
 
 program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .argument('filepath1')
-    .argument('filepath2')
-    .option('-f, --format <type>', 'output format');
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .argument('filepath1')
+  .argument('filepath2')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
-
