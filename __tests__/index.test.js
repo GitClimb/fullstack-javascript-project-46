7d907467ead6f54591a8ee64cf314aabd48e7466
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-10-21 20:29:23 +0800

    refactor: add test and lint GitHub Actions

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,17 +1,18 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
-import genDiff from '../src/index.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { test, expect } from '@jest/globals';
+import genDiff from '../src/index.js';
 
 test('gendiff', () => {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
 
-    const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-    const readFile = (filename) => JSON.parse(fs.readFileSync(getFixturePath(filename)));
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const readFile = (filename) => JSON.parse(fs.readFileSync(getFixturePath(filename)));
 
-    expect(genDiff(readFile('testFile1.json'), readFile('testFile2.json'))).toEqual(`{
+  expect(genDiff(readFile('testFile1.json'), readFile('testFile2.json'))).toEqual(`{
   - follow: false 
     host: hexlet.io 
   - proxy: 123.234.53.22 
