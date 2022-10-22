01b8bcbf7641b74a465b2a22bc6d4a778340844e
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-10-22 18:10:39 +0800

    modified: test index.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,16 @@
-import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
+import parseFile from '../src/parsers.js';
 
 test('gendiff', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
 
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-  const readFile = (filename) => JSON.parse(fs.readFileSync(getFixturePath(filename)));
+  const readFile = (filename) => parseFile(getFixturePath(filename));
 
   expect(genDiff(readFile('testFile1.json'), readFile('testFile2.json'))).toEqual(`{
   - follow: false 
