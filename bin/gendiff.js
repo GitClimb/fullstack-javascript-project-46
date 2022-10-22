65a744877c1931c59149e8dbede9704050620fb7
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-10-22 17:57:28 +0800

    feat: add parsers

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,8 @@
 #! /usr/bin/env node
 
 import { program } from 'commander';
-import * as fs from 'node:fs';
-import * as path from 'node:path';
 import genDiff from '../src/index.js';
+import parseFile from '../src/parsers.js';
 
 program
   .version('0.0.1')
@@ -12,11 +11,9 @@ program
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => {
-    const file1 = path.resolve(process.cwd(), filepath1);
-    const file2 = path.resolve(process.cwd(), filepath2);
-    const readFile1 = JSON.parse(fs.readFileSync(file1));
-    const readFile2 = JSON.parse(fs.readFileSync(file2));
-    console.log(genDiff(readFile1, readFile2));
+    const file1 = parseFile(filepath1);
+    const file2 = parseFile(filepath2);
+    console.log(genDiff(file1, file2));
   });
 
 program.parse();
---
+++ src/parsers.js
@@ -0,0 +1,16 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import yaml from 'js-yaml';
+
+const parseFile = (filepath) => {
+  const configPath = path.resolve(process.cwd(), filepath);
+  const format = path.extname(configPath);
+  const data = fs.readFileSync(configPath);
+
+  if (format === '.yaml') {
+    return yaml.load(data);
+  }
+  return JSON.parse(data);
+};
+
+export default parseFile;
