5bd0acb27e876098d220fdcdd7fced62e45745e8
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-10-28 18:13:55 +0800

    feat: genDiff modified

--- src/index.js
+++ src/index.js
@@ -8,28 +8,61 @@ const compare = (coll1, coll2) => {
   ollKeys.filter((part) => (!uniqueKeys.includes(part) ? uniqueKeys.push(part) : part));
 
   const diff = uniqueKeys.reduce((acc, key) => {
-    if (keysColl1.includes(key) && !keysColl2.includes(key)) {
-      acc.push(`  - ${key}: ${coll1[key]}`);
-    } else if (!keysColl1.includes(key) && keysColl2.includes(key)) {
-      acc.push(`  + ${key}: ${coll2[key]}`);
-    } else if (coll1[key] > coll2[key]) {
-      acc.push(`  - ${key}: ${coll1[key]}`);
-      acc.push(`  + ${key}: ${coll2[key]}`);
-    } else if (coll1[key] < coll2[key]) {
-      acc.push(`  + ${key}: ${coll1[key]}`);
-      acc.push(`  - ${key}: ${coll2[key]}`);
-    } else if (keysColl1.includes(key) === keysColl2.includes(key)) {
-      acc.push(`    ${key}: ${coll2[key]}`);
+    if (_.isObject(coll1[key]) && _.isObject(coll2[key])) {
+      acc[`${key}`] = compare(coll1[key], coll2[key]);
+    }
+    if (!keysColl2.includes(key)) {
+      acc[`- ${key}`] = coll1[key];
+    }
+    if (!keysColl1.includes(key)) {
+      acc[`+ ${key}`] = coll2[key];
+    }
+    if (keysColl1.includes(key) && keysColl2.includes(key)) {
+      if (coll1[key] > coll2[key]) {
+        acc[`- ${key}`] = coll1[key];
+        acc[`+ ${key}`] = coll2[key];
+      }
+      if (coll1[key] < coll2[key]) {
+        acc[`- ${key}`] = coll1[key];
+        acc[`+ ${key}`] = coll2[key];
+      }
+      if (coll1[key] === coll2[key]) {
+        acc[`${key}`] = coll1[key];
+      }
     }
     return acc;
-  }, []);
+  }, {});
 
   return diff;
 };
 
+const stringify = (value, replacer = ' ', spacesCount = 1) => {
+  const iter = (currentValue, detph) => {
+    if (!Object.is(typeof currentValue, typeof []) || currentValue === null) {
+      return `${currentValue}`;
+    }
+
+    const indentSize = detph * spacesCount;
+    const frontIndent = replacer.repeat(indentSize - 1);
+    const rearIndent = replacer.repeat(indentSize - spacesCount);
+    const lines = Object
+      .entries(currentValue)
+      .map(([key, val]) => {
+        if (key.startsWith('+ ') || key.startsWith('- ')) {
+          return `${frontIndent}${key}: ${iter(val, detph + 1)}`;
+        }
+        return `${frontIndent}  ${key}: ${iter(val, detph + 1)}`;
+      });
+    return ['{', ...lines, `${rearIndent}}`].join('\n');
+  };
+
+  return iter(value, 1);
+};
+
 const genDiff = (file1, file2) => {
   const compareFiles = compare(file1, file2);
-  return `{\n${compareFiles.join(' \n')}\n}`;
+  const stringy = stringify(compareFiles, '  ', 2);
+  return stringy;
 };
 
 export default genDiff;
