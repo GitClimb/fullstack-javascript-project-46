f87ac95630ca8b84e09a8d0ddf4b92865c4373f1
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-11-01 19:11:35 +0800

    feat: parsers yml format

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import compare from './compare.js';
 import parseFile from './parsers.js';
 
 const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
-  const file1 = parseFile(filePath1);
-  const file2 = parseFile(filePath2);
-  const compareFiles = compare(file1, file2);
+  const fil1 = parseFile(filePath1);
+  const fil2 = parseFile(filePath2);
+  const compareFiles = compare(fil1, fil2);
   const stringy = selectFormat(compareFiles, formatName);
   return stringy;
 };
