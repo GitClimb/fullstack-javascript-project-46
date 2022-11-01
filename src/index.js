16b88eaf308b0b890618cb7315399a9085da7c28
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-11-01 18:44:38 +0800

    feat: refactor main

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import selectFormat from './formatters/index.js';
 import compare from './compare.js';
 
-const genDiff = (file1, file2, formatName) => {
+const genDiff = (file1, file2, formatName = 'stylish') => {
   const compareFiles = compare(file1, file2);
   const stringy = selectFormat(compareFiles, formatName);
   return stringy;
