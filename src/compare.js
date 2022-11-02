ebe01422c6d4450daeb18e832e580e06c3e142a9
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-11-02 11:03:01 +0800

    build: modified compare func

--- src/compare.js
+++ src/compare.js
@@ -4,29 +4,28 @@ const compare = (coll1, coll2) => {
   const keysColl1 = _.keys(coll1);
   const keysColl2 = _.keys(coll2);
   const ollKeys = _.sortBy(keysColl1.concat(keysColl2));
-  const uniqueKeys = ollKeys
-    .filter((el, id) => ollKeys.indexOf(el) === id)
-    .reduce((acc, key) => {
-      const result = { ...acc };
-      if (_.isObject(coll1[key]) && _.isObject(coll2[key])) {
-        result[`${key}`] = compare(coll1[key], coll2[key]);
-      } else if (!keysColl2.includes(key)) {
-        result[`- ${key}`] = coll1[key];
-      } else if (!keysColl1.includes(key)) {
-        result[`+ ${key}`] = coll2[key];
-      } else {
-        if (coll1[key] !== coll2[key]) {
-          result[`- ${key}`] = coll1[key];
-          result[`+ ${key}`] = coll2[key];
-        }
-        if (coll1[key] === coll2[key]) {
-          result[`${key}`] = coll1[key];
-        }
-      }
-      return result;
-    }, {});
+  const uniqueKeys = ollKeys.filter((el, id) => ollKeys.indexOf(el) === id);
 
-  return uniqueKeys;
+  const diff = uniqueKeys.reduce((acc, key) => {
+    if (_.isObject(coll1[key]) && _.isObject(coll2[key])) {
+      acc[`${key}`] = compare(coll1[key], coll2[key]);
+    } else if (!keysColl2.includes(key)) {
+      acc[`- ${key}`] = coll1[key];
+    } else if (!keysColl1.includes(key)) {
+      acc[`+ ${key}`] = coll2[key];
+    } else {
+      if (coll1[key] !== coll2[key]) {
+        acc[`- ${key}`] = coll1[key];
+        acc[`+ ${key}`] = coll2[key];
+      }
+      if (coll1[key] === coll2[key]) {
+        acc[`${key}`] = coll1[key];
+      }
+    }
+    return acc;
+  }, {});
+
+  return diff;
 };
 
 export default compare;
