990ad8ca6a5c5fd3deaf3544e70a58cac236d2d3
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-11-02 15:07:28 +0800

    feat: modificate compare

--- src/compare.js
+++ src/compare.js
@@ -7,20 +7,19 @@ const compare = (coll1, coll2) => {
   const uniqueKeys = ollKeys.filter((el, id) => ollKeys.indexOf(el) === id);
 
   const diff = uniqueKeys.reduce((acc, key) => {
-    const result = { ...acc };
     if (_.isObject(coll1[key]) && _.isObject(coll2[key])) {
-      result[`${key}`] = compare(coll1[key], coll2[key]);
+      acc[`${key}`] = compare(coll1[key], coll2[key]);
     } else if (!keysColl2.includes(key)) {
-      result[`- ${key}`] = coll1[key];
+      acc[`- ${key}`] = coll1[key];
     } else if (!keysColl1.includes(key)) {
-      result[`+ ${key}`] = coll2[key];
+      acc[`+ ${key}`] = coll2[key];
     } else if (coll1[key] !== coll2[key]) {
-      result[`- ${key}`] = coll1[key];
-      result[`+ ${key}`] = coll2[key];
+      acc[`- ${key}`] = coll1[key];
+      acc[`+ ${key}`] = coll2[key];
     } else {
-      result[`${key}`] = coll1[key];
+      acc[`${key}`] = coll1[key];
     }
-    return result;
+    return { ...acc };
   }, {});
 
   return diff;
