878b6202638cc9c351ddc1d86a7efad439b04ebd
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-11-02 19:25:15 +0800

    feat: modificate compare

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -15,24 +15,26 @@ const type = (part) => {
 
 const plain = (values, path = '') => {
   const keys = Object.keys(values);
-  let result = '';
+  const result = [];
 
   for (let i = 0; i < keys.length; i += 1) {
     const part = keys[i];
     let fill = path;
+
     if (!part.startsWith('-') && !part.startsWith('+') && _.isObject(values[part])) {
       fill = `${fill}${part}.`;
-      result = `${result}${plain(values[part], fill)}\n`;
+      result.push(`${plain(values[part], fill)}`);
     } else if (keys[i + 1] !== undefined && part.slice(2) === keys[i + 1].slice(2)) {
-      result = `${result}Property '${path}${part.slice(2)}' was updated. From ${type(isSost(values[part]))} to ${type(isSost(values[keys[i + 1]]))}\n`;
+      const partt = `Property '${path}${part.slice(2)}' was updated. From ${type(isSost(values[part]))} to ${type(isSost(values[keys[i + 1]]))}`;
       i += 1;
+      result.push(partt);
     } else if (part.startsWith('+ ')) {
-      result = `${result}Property '${path}${part.slice(2)}' was added with value: ${type(isSost(values[part]))}\n`;
+      result.push(`Property '${path}${part.slice(2)}' was added with value: ${type(isSost(values[part]))}`);
     } else if (part.startsWith('- ')) {
-      result = `${result}${`Property '${path}${part.slice(2)}' was removed`}\n`;
+      result.push(`Property '${path}${part.slice(2)}' was removed`);
     }
   }
-  return result.slice(0, result.length - 1);
+  return result.join('\n');
 };
 
 export default plain;
